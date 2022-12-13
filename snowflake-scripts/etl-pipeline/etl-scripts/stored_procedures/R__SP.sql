6008e47f84ec6858bb0f311df47e7f76c267a288
shivakrishn07 <83999563+shivakrishn07@users.noreply.github.com>
2022-12-13 15:13:46 +0530

    Update R__SP.sql

--- snowflake-scripts/etl-pipeline/etl-scripts/stored_procedures/R__SP.sql
+++ snowflake-scripts/etl-pipeline/etl-scripts/stored_procedures/R__SP.sql
@@ -1,1 +1,13 @@
+use schema new_rent;
+create or replace procedure demo_sp()
+returns string
+language sql
+as
+$$
+begin
+insert into new_rent_table values('shiva123@gmail.com','94400000202');
+return 'success';
+end;
+$$;
+
 
